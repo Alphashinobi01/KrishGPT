e8e792c3cbc870f480dc46a191926d6cd31726d8
Aditya  Pawar <134551339+Alphashinobi01@users.noreply.github.com>
2025-11-27 00:00:18 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/KrishGPT/",
   plugins: [react()],
   define: {
     'process.env.API_KEY': JSON.stringify("AIzaSyBPGoXGcC8rwHw73-h56pjrnwZugxBGqo8")
